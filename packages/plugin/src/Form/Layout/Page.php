1d19edbe91742b161903f6aba092675d860346a8
Gustavs Gūtmanis <gustavs-gutmanis@users.noreply.github.com>
2024-10-31 22:27:13 +0200

    [SFT-1431]: Added ability to render captcha placement manually (#1580)

--- packages/plugin/src/Form/Layout/Page.php
+++ packages/plugin/src/Form/Layout/Page.php
@@ -92,6 +92,11 @@ class Page implements \IteratorAggregate
         return $this->buttons;
     }
 
+    public function isFirst(): bool
+    {
+        return 0 === $this->index;
+    }
+
     public function getIterator(): \ArrayIterator
     {
         return $this->layout->getRows()->getIterator();
