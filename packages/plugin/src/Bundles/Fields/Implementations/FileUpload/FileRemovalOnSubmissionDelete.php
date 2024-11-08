3ffed7fe3ea190d10640108e97d9b1c7f105c755
Gustavs Gūtmanis <gustavs-gutmanis@users.noreply.github.com>
2024-11-08 21:23:52 +0200

    fix(SFT-1536): remove assets when submissions deleted in Submissions index

---
+++ packages/plugin/src/Bundles/Fields/Implementations/FileUpload/FileRemovalOnSubmissionDelete.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Solspace\Freeform\Bundles\Fields\Implementations\FileUpload;
+
+use Solspace\Freeform\Elements\Submission;
+use Solspace\Freeform\Fields\Interfaces\FileUploadInterface;
+use Solspace\Freeform\Library\Bundles\FeatureBundle;
+use yii\base\Event;
+
+class FileRemovalOnSubmissionDelete extends FeatureBundle
+{
+    public function __construct()
+    {
+        Event::on(
+            Submission::class,
+            Submission::EVENT_AFTER_DELETE,
+            [$this, 'removeSubmissionFiles']
+        );
+    }
+
+    public function removeSubmissionFiles(Event $event): void
+    {
+        /** @var Submission $submission */
+        $submission = $event->sender;
+        $fields = $submission->getFieldCollection()->getList(FileUploadInterface::class);
+
+        foreach ($fields as $field) {
+            $value = $submission->getFormFieldValue($field);
+
+            if (\is_array($value)) {
+                foreach ($value as $id) {
+                    \Craft::$app->elements->deleteElementById($id);
+                }
+            }
+        }
+    }
+}
