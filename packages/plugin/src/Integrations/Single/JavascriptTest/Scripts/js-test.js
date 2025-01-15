4e83dee714da87b441cc26f675b5a2dbc31d87f3
kjmartens <kelsey@solspace.com>
2025-01-15 00:29:08 -0600

    feat: minifying all sample template CSS and JS, including JS Test JS

--- packages/plugin/src/Integrations/Single/JavascriptTest/Scripts/js-test.js
+++ packages/plugin/src/Integrations/Single/JavascriptTest/Scripts/js-test.js
@@ -1,12 +1,1 @@
-var jsTest = function (event) {
-  var form = event.form;
-  var input = form.querySelector('[data-ff-check] + input[name]');
-  if (!input) {
-    return;
-  }
-
-  input.value = '';
-}
-
-document.addEventListener('freeform-ready', jsTest);
-document.addEventListener('freeform-ajax-after-submit', jsTest);
+var jsTest=function(e){var t=e.form.querySelector("[data-ff-check] + input[name]");t&&(t.value="")};document.addEventListener("freeform-ready",jsTest),document.addEventListener("freeform-ajax-after-submit",jsTest);
