7b92f6f2eb741f35dc316c0053c7fbe57c68f325
thejahid <thejahid@icloud.com>
2025-02-07 00:57:21 +0600

    feat(SFT-1608): Implement Form Monitor stats and UI improvements

--- packages/client/src/app/pages/form-monitor/form-monitor.styles.tsx
+++ packages/client/src/app/pages/form-monitor/form-monitor.styles.tsx
@@ -1,9 +1,11 @@
 import type { PropsWithChildren } from 'react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Breadcrumb } from '@components/breadcrumbs/breadcrumbs';
 import { ContentContainer } from '@components/layout/blocks/content-container';
 import { HeaderContainer } from '@components/layout/blocks/header-container';
 import { colors } from '@ff-client/styles/variables';
+import { spacings } from '@ff-client/styles/variables';
 import translate from '@ff-client/utils/translations';
 import styled from 'styled-components';
 
@@ -22,7 +24,7 @@ export const FormMonitorWrapper: React.FC<PropsWithChildren> = ({
 
       <div id="main-content">
         <ContentContainer>
-          <div id="content">{children}</div>
+          <LayoutContainer id="content">{children}</LayoutContainer>
         </ContentContainer>
       </div>
     </div>
@@ -65,5 +67,93 @@ export const StatusBadge = styled.div`
 
   &.status-failed {
     background-color: ${colors.red500};
+  }
+`;
+
+export const Cards = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: ${spacings.lg};
+`;
+
+export const Card = styled(Link)`
+  border: 1px solid ${colors.gray200};
+  border-radius: 5px;
+
+  &:hover {
+    background-color: ${colors.gray050};
+    text-decoration: none;
+  }
+`;
+
+export const CardContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-radius: 5px;
+`;
+
+export const FormCardContent = styled.div`
+  padding: ${spacings.xl} ${spacings.xl};
+`;
+
+export const Title = styled.h2`
+  cursor: pointer;
+
+  color: #3d464e;
+
+  font-size: 20px;
+  font-weight: 700;
+  text-align: left;
+
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  transition: all 0.2s ease-out;
+
+  &:hover {
+    border: none;
+  }
+`;
+
+export const StatsChartContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${spacings.md};
+  padding: ${spacings.md};
+`;
+
+export const Legend = styled.div`
+  display: flex;
+  gap: ${spacings.lg};
+  margin-top: ${spacings.md};
+`;
+
+export const LegendItem = styled.div<{ color: string }>`
+  display: flex;
+  align-items: center;
+  gap: ${spacings.xs};
+
+  &:before {
+    content: '';
+    display: block;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+  }
+`;
+
+export const LayoutContainer = styled.div`
+  gap: ${spacings.xl};
+  align-items: flex-start;
+
+  ${Cards} {
+    flex: 2;
+  }
+
+  ${StatsChartContainer} {
+    position: sticky;
+    top: ${spacings.xl};
   }
 `;
